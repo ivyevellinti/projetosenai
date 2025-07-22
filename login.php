ed7552647209ac3c29bbbba012b24d5758124e44
MatheusBatista07 <bomfimmatheus07@gmail.com>
2025-07-22 10:51:47 -0300

    feature-login

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HandCare | Login</title>
+    <link rel="stylesheet" href="lg.css">
+</head>
+<body>
+        <img class="hc" src="img/HandCare.png" alt="Imagem HandCare">
+    <div class="formulario">
+        <img src="img/HandCareHorizontal.png" alt="Símbolo HandCare">
+        <form method="POST" class="forms">
+            <label for="userCPF" class="userCPF">CPF</label>
+            <input name="userCPF" type="text" id="userCPF" placeholder="Seu CPF">
+            <label for="userPassword" class="userPassword">Senha</label>
+            <input name="userPassword" type="text" id="userPassword" placeholder="Sua Senha">
+            <button class="cad-se">Cadastre-se</button>
+            <button>Esqueceu a senha?</button>
+            <button>Entre com o Google</button>
+            <button class="entrar">Entrar</button>
+        </form>
+    </div>
+</body>
+</html>
