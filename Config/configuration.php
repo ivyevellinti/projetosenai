b299341c42446357d353fd80f75892b42ea40718
ivyevellin.ti <ivy.evellin01@gmail.com>
2025-07-22 09:00:33 -0300

    instalação do composer

---
+++ Config/configuration.php
@@ -0,0 +1,11 @@
+<?php
+//CONFIGURAÇÃO DO BANCO DE DADOS
+
+define( "DB_NAME",  "clinica");
+define( "DB_HOST",  "localhost");
+define( "DB_USER",  "root");
+define( "DB_PASSWORD",  '');
+define( "DB_PORT",  "3306");
+
+
+?>
