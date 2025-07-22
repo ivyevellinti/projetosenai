5fad2df844610d5e67d9b029134d7e305ece4aff
Anna Luísa <annaluisateixeira8@gmail.com>
2025-07-22 11:19:22 -0300

    feat: Construção da página inicial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../projetosenai/index.css">
+    <title>HandCare</title>
+</head>
+<body>
+    <!-- 001768 -->
+     <main>
+        <div class="conteudo">
+            <figure>
+                <img src="../projetosenai/img/Group 17.png" alt="Simbolo da empresa">
+            </figure>
+            <button class="cadastrar">Cadastre-se</button>
+            <button class="entrar">Entrar</button>
+        </div>
+     </main>
+</body>
+</html>
