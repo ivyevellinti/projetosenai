f94bccc11bf0e6fec810b19a808d3a230603a740
Anna Luísa <annaluisateixeira8@gmail.com>
2025-07-22 09:26:31 -0300

    feat: início da página de cadastro

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <main>
+        <figure>
+            <img src="../projetosenai/img/simbolo-handcare.png" alt="Simbolo da empresa">
+        </figure>
+        <h1>Cadastre-se</h1>
+        <p>Crie uma conta, para aproveitar o HandCare</p>
+        <form method="POST">
+            
+        </form>
+    </main>
+    
+</body>
+</html>
