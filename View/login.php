ab8496386596f1607f524a7df01f61158c4af497
ivyevellin.ti <ivy.evellin01@gmail.com>
2025-07-22 11:44:08 -0300

    reorganização das páginas

--- View/login.php
+++ View/login.php
@@ -1,20 +1,21 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HandCare | Login</title>
-    <link rel="stylesheet" href="lg.css">
+    <link rel="stylesheet" href="../template/lg.css">
 </head>
 <body>
-        <img class="hc" src="img/HandCare.png" alt="Imagem HandCare">
+        <img class="hc" src="../img/HandCare.png" alt="Imagem HandCare">
     <div class="formulario">
-        <img src="img/HandCareHorizontal.png" alt="Símbolo HandCare">
+        <img src="../img/HandCareHorizontal.png" alt="Símbolo HandCare">
         <form method="POST" class="forms">
             <label for="userCPF" class="userCPF">CPF</label>
-            <input name="userCPF" type="text" id="userCPF" placeholder="Seu CPF">
+            <input name="cpf" type="text" id="userCPF" placeholder="Seu CPF">
             <label for="userPassword" class="userPassword">Senha</label>
-            <input name="userPassword" type="text" id="userPassword" placeholder="Sua Senha">
+            <input name="password" type="text" id="userPassword" placeholder="Sua Senha">
             <button class="cad-se">Cadastre-se</button>
             <button>Esqueceu a senha?</button>
             <button>Entre com o Google</button>
